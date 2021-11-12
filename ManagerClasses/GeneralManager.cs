67518f483be95f2f6568b42e5b28b4dca6befe91
Jynsaillar <Jynsaillar@ymail.com>
2021-11-12 12:21:37 +0100

    Added method to parse boss data as string from a JSON object

--- ManagerClasses/GeneralManager.cs
+++ ManagerClasses/GeneralManager.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 
 namespace Boss_Timer_Overlay.ManagerClasses
 {
@@ -47,6 +49,61 @@ namespace Boss_Timer_Overlay.ManagerClasses
             return bossData;
         }
         */
+
+        public static string ParseBossDataFromJObject(JObject jobject)
+        {
+            dynamic bossData = jobject;
+
+            // Local function for lazily listing all days in the JSON object (if they exist)
+            IEnumerable<string> getNameOfDays()
+            {
+                foreach (var day in (JObject)jobject["days"])
+                {
+                    yield return day.Key;
+                }
+            }
+
+            if ((JObject)jobject["days"] is null)
+                return string.Empty;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var days = (JObject)jobject["days"];
+
+            foreach (var day in days)
+            {
+                stringBuilder.AppendLine(day.Key);
+
+                foreach (var timeSlot in (JObject)days[day.Key])
+                {
+                    stringBuilder.AppendLine(timeSlot.Key);
+
+                    foreach (var boss in (JArray)timeSlot.Value)
+                    {
+                        stringBuilder.AppendLine(boss.Value<string>());
+                    }
+                }
+
+                stringBuilder.AppendLine();
+                /*
+                foreach (var timeSlot in (JArray)day.Value)
+                {
+                    stringBuilder.AppendLine(timeSlot.Value<string>());
+
+                    foreach (var boss in (JObject)jobject["days"][day.Key][timeSlot.Key])
+                    {
+                        stringBuilder.AppendLine(boss.Key);
+                    }
+                }
+                */
+            }
+
+            return stringBuilder.ToString();
+
+            //return string.Join(",", getNameOfDays());
+
+            return string.Empty;
+        }
 
         public static string BossDataToInfoString(BossData bossData)
         {
