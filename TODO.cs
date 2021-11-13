cde4cfdd35b990ceb223e3128bd0ebc64c9265d3
Jynsaillar <Jynsaillar@ymail.com>
2021-11-13 13:22:12 +0100

    Added central TODO.cs file which holds general //todo: comments, these should be displayed in the Task List in Visual Studio, for example - the idea is to note down planned features in a central place

---
+++ TODO.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * This 'class' serves no purpose other than to hold general overarching //todo: comments,
+ * since some plugins parse comments containing //todo or some variation.
+ * In visual studio, // TODO: in C# files will (should) appear in the Task List.
+ */
+
+namespace Boss_Timer_Overlay
+{
+    //todo: Add screenshot preview of UI to let the user properly place the overlay on their screen (variable resolutions)
+    //todo: Don't load bitmaps from disk e v e r y time the UI updates (memory isn't free), instead load the boss images on startup and then ref them on update
+}
