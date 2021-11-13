dbc8fb33b9aa7bb281b194e64ec5a33f02b514b9
Jynsaillar <Jynsaillar@ymail.com>
2021-11-13 23:22:41 +0100

    Removed the separate BossOverlaySettings as it made no sense logically and added a globally accessible class BossInfo from which a list of boss names and their respective ids can be retrieved

---
+++ StaticData/BossInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Boss_Timer_Overlay.StaticData
+{
+    public static class BossInfo
+    {
+        public static System.Collections.ObjectModel.ReadOnlyCollection<string> Bosses = new System.Collections.Generic.List<string>() {
+            "None",
+            "Kzarka",
+            "Karanda",
+            "Kutum",
+            "Nouver",
+            "Offin",
+            "Garmoth",
+            "Vell",
+            "Quint",
+            "Muraka",
+            "Black Shadow",
+            "Event"
+            }.AsReadOnly();
+
+        public static int GetBossIdFromName(string bossName)
+        {
+            int id = Bosses
+                .ToList()
+                .FindIndex(x => x.Equals(bossName, System.StringComparison.OrdinalIgnoreCase));
+
+            return (id >= 0) ? id : 0; // Return the id for "None" = 0 by default, since List.IndexOf(...) returns -1 if no match is found
+        }
+    }
+}
